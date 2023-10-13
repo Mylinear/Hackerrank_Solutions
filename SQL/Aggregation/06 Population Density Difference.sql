a0f95f6ef203c5462f96b0a0bccb805603b30e56
Mustafa Yasin Gunduz <linearthecoder@gmail.com>
2023-10-13 13:40:08 +0300

    Create 06 Population Density Difference.sql

---
+++ SQL/Aggregation/06 Population Density Difference.sql
@@ -0,0 +1,7 @@
+Question link : https://www.hackerrank.com/challenges/population-density-difference/problem?isFullScreen=true
+
+Language: MySQL
+
+  
+select max(population) - min(population)
+from city
